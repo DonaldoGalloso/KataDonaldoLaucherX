abf9fea43093645ea7dfb6006fe53db14f2061a9
Luis Donaldo Galloso <40219825+DonaldoGalloso@users.noreply.github.com>
2022-02-14 07:03:56 -0600

    Katas

---
+++ primerPrograma.py
@@ -0,0 +1,6 @@
+from datetime import date
+print("la fecha de hoy es "+ str(date.today()))
+
+parsec = 11
+lightyears = parsec * 3.26156
+print( str(parsec) + " parsec, is " + str(lightyears) + " lightyears" )
